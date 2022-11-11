3b30e74dc1a58c43ceae83e9240cf3815051db47
o.lobanov <o.lobanov@crpt.ru>
2022-11-11 21:22:31 +0300

    11.11.2022 Intersection Of Arrays

--- src/sorting/MergeSort.java
+++ src/sorting/MergeSort.java
@@ -14,51 +14,6 @@ public class MergeSort {
         System.out.println("in place merge sort :" + Arrays.toString(arr1));
     }
 
-    public static int[] intersect(int[] nums1, int[] nums2) {
-        mergeSort(nums1);
-        mergeSort(nums2);
-
-        int[] l;
-        int[] s;
-
-        if (nums1.length > nums2.length) {
-            l = nums1;
-            s = nums2;
-        } else {
-            l = nums2;
-            s = nums1;
-        }
-
-        int longIndex = 0;
-        int[] res = new int[s.length];
-        int k = 0;
-        int m =0;
-        for (int i = 0; i < s.length; i++) {
-            m = longIndex;
-            while (m < l.length) {
-                if (s[i] == l[m]) {
-                    res[k] = s[i];
-                    k++;
-                    longIndex = ++m;
-                    break;
-                }
-                m++;
-            }
-            if (longIndex > l.length - 1) {
-                break;
-            }
-        }
-        if (k == 0) {
-            return new int[0];
-        } else {
-            int[] trimmedRes = new int[k];
-            for (int i = 0; i <= trimmedRes.length - 1; i++) {
-                trimmedRes[i] = res[i];
-            }
-            return trimmedRes;
-        }
-    }
-
 
     public static void mergeSort(int[] arr) {
         if (arr == null || arr.length <= 1) {
